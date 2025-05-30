4cf6db2200df2aa67167a371b384c1fbcbeb8799
spatkar94 <37498091+spatkar94@users.noreply.github.com>
2025-05-30 18:15:40 -0400

    Delete HistoTME_regression/run_cptac_validations.sh

--- HistoTME_regression/run_cptac_validations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ctype=("LUAD" "LSCC" "HNSCC" "UCEC" "CCRCC" "PDA" "GBM")
-fm=("uni" "uni2" "virchow" "virchow2" "gigapath" "hoptimus0")
-for item1 in "${ctype[@]}"; do
-    for item2 in "${fm[@]}"; do
-        python gen_CPTAC_predictions.py --cohort CPTAC --cancer_type $item1 --embed $item2
-    done 
-done 
