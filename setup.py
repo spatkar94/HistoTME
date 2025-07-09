7e9b489dadfb40562a7cabb7804d728e04b7e642
spatkar94 <37498091+spatkar94@users.noreply.github.com>
2025-07-09 14:42:24 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name='uni',
+    version='0.1.0',
+    description='HistoTME',
+    url='https://github.com/spatkar/HistoTME',
+    author='SP, AC',
+    author_email='',
+    license='CC BY-NC 4.0',
+    packages=find_packages(exclude=['__dep__', 'assets']),
+    install_requires=["torch>=2.0.1", "torchvision", "timm==0.9.8", 
+                      "numpy<2", "pandas", "scikit-learn", "tqdm",
+                      "transformers","opencv-python","openslide-python",
+                      "openslide-bin"],
+
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: CC BY-NC 4.0",
+]
+)
